cd25bd23f0ff136bbfe5eb17801b7bfb8cea08ee
tarunmalladi <malladitarun100@gmail.com>
2026-02-26 12:09:50 +0530

    multi-cloud

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "terraform-state-bucket-aw"
     key            = "multi-cloud-deployement/terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "terraform-state-lock"
+    #dynamodb_table = "terraform-state-lock"
     encrypt        = true
   }
 }
