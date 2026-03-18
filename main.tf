4f31591fb1bc1ee5ac5f98cc318495fc206b6fab
tarunmalladi <malladitarun100@gmail.com>
2026-03-18 15:43:55 +0530

    aws code pushed

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 ################################
 resource "aws_instance" "aws_nginx" {
   ami           = "ami-0f58b397bc5c1f2e8" # Amazon Linux 2 (ap-south-1)
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   user_data = <<-EOF
               #!/bin/bash
